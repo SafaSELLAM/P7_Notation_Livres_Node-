ce39842bae3d495dec26ece9cd5285e8591893d9
SafaSELLAM <safa.sellam@outlook.fr>
2023-05-05 12:45:00 +0200

    docs : optimisation ajout image avec sharp

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
@@ -16,4 +18,34 @@ const storage = multer.diskStorage({
   },
 });
 
+const fileFilter = (req, file, callback) => {
+  if (MIME_TYPES[file.mimetype]) {
+    callback(null, true);
+  } else {
+    callback(new Error("Invalid file type"));
+  }
+};
+
+const upload = multer({ storage, fileFilter }).single("image");
+
+exports.uploadImage = (req, res, next) => {
+  upload(req, res, async (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(400).json({ error: "File too large" });
+    } else if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+
+    // Redimensionnement et compression de l'image
+    const image = sharp(req.file.path);
+    await image
+      .resize({ width: 800 })
+      .jpeg({ quality: 80 })
+      .toFile(
+        path.join(__dirname, "..", "public", "images", req.file.filename)
+      );
+
+    next();
+  });
+};
 module.exports = multer({ storage }).single("image");
