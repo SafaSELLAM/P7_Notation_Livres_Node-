c5f06761aab67b1cdefaac13e691c97e2f436f44
SafaSELLAM <safa.sellam@outlook.fr>
2023-05-09 13:34:41 +0200

    test: resize images

--- Routes/book.js
+++ Routes/book.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+// const { upload, resizeImage } = require("../middleware/multer-config");
 const multer = require("../middleware/multer-config");
 const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/bestrating", bookCtrl.getRatings);
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, multer.upload, multer.resizeImage, bookCtrl.createBook);
+// router.post("/", auth, upload, resizeImage, bookCtrl.createBook);
 router.get("/:id", bookCtrl.getOneBook);
 
-router.put("/:id", auth, multer, bookCtrl.updateBook);
+router.put(
+  "/:id",
+  auth,
+  multer.upload,
+  multer.resizeImage,
+  bookCtrl.updateBook
+);
+// router.put("/:id", auth, upload, resizeImage, bookCtrl.updateBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.post("/:id/rating", auth, bookCtrl.postRating);
 
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,6 +1,25 @@
-const multer = require("multer");
 const sharp = require("sharp");
-const path = require("path");
+const multer = require("multer");
+
+// const MIME_TYPES = {
+//   "image/jpg": "jpg",
+//   "image/jpeg": "jpg",
+//   "image/png": "png",
+// };
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "images");
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(" ").join("_");
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + "." + extension);
+//   },
+// });
+
+// module.exports = multer({ storage: storage }).single("image");
+
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
@@ -12,40 +31,34 @@ const storage = multer.diskStorage({
     callback(null, "images");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split("").join("_");
+    const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + "." + extension);
   },
 });
 
-const fileFilter = (req, file, callback) => {
-  if (MIME_TYPES[file.mimetype]) {
-    callback(null, true);
-  } else {
-    callback(new Error("Invalid file type"));
+const upload = multer({ storage: storage }).single("image");
+
+// Middleware pour redimensionner l'image
+const resizeImage = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  try {
+    const imageBuffer = await sharp(req.file.path)
+      .resize(206)
+      .jpeg({ quality: 80 })
+      .toBuffer();
+
+    await sharp(imageBuffer).toFile(`images/${req.file.filename}`);
+    await sharp(imageBuffer).toFile(`images/images_redim/${req.file.filename}`);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error resizing image", error: error });
   }
 };
 
-const upload = multer({ storage, fileFilter }).single("image");
-
-exports.uploadImage = (req, res, next) => {
-  upload(req, res, async (err) => {
-    if (err instanceof multer.MulterError) {
-      return res.status(400).json({ error: "File too large" });
-    } else if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-
-    // Redimensionnement et compression de l'image
-    const image = sharp(req.file.path);
-    await image
-      .resize({ width: 800 })
-      .jpeg({ quality: 80 })
-      .toFile(
-        path.join(__dirname, "..", "public", "images", req.file.filename)
-      );
-
-    next();
-  });
-};
-module.exports = multer({ storage }).single("image");
+module.exports = { upload, resizeImage };
