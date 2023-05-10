44f30c3ed8dc9d9485e3f0a3f3a8dbdafaf7ad9f
SafaSELLAM <safa.sellam@outlook.fr>
2023-05-10 18:23:07 +0200

    desturcturation routes avec multer

--- Routes/book.js
+++ Routes/book.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer-config");
+const { upload, resizeImage } = require("../middleware/multer-config");
 const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/bestrating", bookCtrl.getRatings);
-router.post("/", auth, multer.upload, multer.resizeImage, bookCtrl.createBook);
-router.get("/:id", bookCtrl.getOneBook);
+router.post("/", auth, upload, resizeImage, bookCtrl.createBook);
 
-router.put(
-  "/:id",
-  auth,
-  multer.upload,
-  multer.resizeImage,
-  bookCtrl.updateBook
-);
-// router.put("/:id", auth, upload, resizeImage, bookCtrl.updateBook);
+router.get("/:id", bookCtrl.getOneBook);
+router.put("/:id", auth, upload, resizeImage, bookCtrl.updateBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.post("/:id/rating", auth, bookCtrl.postRating);
 
