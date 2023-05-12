757c9e3823ccfb0904c922462e6944a62d9c4a21
SafaSELLAM <safa.sellam@outlook.fr>
2023-05-12 13:34:32 +0200

    docs : suppression console.log + test req

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
-    console.log(process.env.RANDOM_TOKEN_SECRET);
     const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
