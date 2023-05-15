6cabfdc4770726a1c62a03261f393b219d8affe1
SafaSELLAM <safa.sellam@outlook.fr>
2023-05-15 19:40:38 +0200

    style

--- app.js
+++ app.js
@@ -31,4 +31,5 @@ app.use((req, res, next) => {
 app.use("/api/auth", userRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
+
 module.exports = app;
